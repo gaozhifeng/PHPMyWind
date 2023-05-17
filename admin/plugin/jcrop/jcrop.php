6cdc2663042bfdd635935ff90311f51f4315407b
Ado <duyueping@vip.qq.com>
2023-05-17 16:43:06 +0800

    修改上传组建为HTML5上传

--- admin/plugin/jcrop/jcrop.php
+++ admin/plugin/jcrop/jcrop.php
@@ -1,4 +1,4 @@
-<?php	require_once(dirname(__FILE__).'/../../inc/config.inc.php');
+<?php require_once(dirname(__FILE__) . '/../../inc/config.inc.php');
 
 /*
 **************************
@@ -10,35 +10,34 @@ person: Feng
 
 
 //引入水印文件
-require_once(PHPMYWIND_DATA.'/watermark/watermark.class.php');
+require_once(PHPMYWIND_DATA . '/watermark/watermark.class.php');
 
 
 //初始化参数
-$imgurl    = isset($imgurl) ? $imgurl : '';
-$srcfile   = PHPMYWIND_ROOT.'/'.$imgurl;
-$newwidth  = isset($iw) ? $iw : '';
+$imgurl = isset($imgurl) ? $imgurl : '';
+$srcfile = PHPMYWIND_ROOT . '/' . $imgurl;
+$newwidth = isset($iw) ? $iw : '';
 $newheight = isset($ih) ? $ih : '';
-$x1        = isset($x1) ? $x1 : '';
-$y1        = isset($y1) ? $y1 : '';
-$x2        = isset($x2) ? $x2 : '';
-$y2        = isset($y2) ? $y2 : '';
-$wm        = isset($wm) ? $wm : '';
+$x1 = isset($x1) ? $x1 : '';
+$y1 = isset($y1) ? $y1 : '';
+$x2 = isset($x2) ? $x2 : '';
+$y2 = isset($y2) ? $y2 : '';
+$wm = isset($wm) ? $wm : '';
 
 
 //获取图片信息
 $srcinfo = @getimagesize($srcfile);
-$width   = $srcinfo[0];
-$height  = $srcinfo[1];
+$width = $srcinfo[0];
+$height = $srcinfo[1];
 
 
 //检测图片扩展名
-if($srcinfo[2] != 1 &&
-   $srcinfo[2] != 2 &&
-   $srcinfo[2] != 3 &&
-   $srcinfo[2] != 6)
-{
-	echo '不符合的图片格式！';
-	exit();
+if ($srcinfo[2] != 1 &&
+    $srcinfo[2] != 2 &&
+    $srcinfo[2] != 3 &&
+    $srcinfo[2] != 6) {
+    echo '不符合的图片格式！';
+    exit();
 }
 
 
@@ -47,23 +46,22 @@ $newimg = @imagecreatetruecolor($newwidth, $newheight);
 
 
 //获取图像文件
-switch($srcinfo[2])
-{
-	case 1:
-		$imgfrom = imagecreatefromgif($srcfile);
-		break;
-	case 2:
-		$imgfrom = imagecreatefromjpeg($srcfile);
-		break;
-	case 3:
-		$imgfrom = imagecreatefrompng($srcfile);
-		break;
-	case 6:
-		$imgfrom = imagecreatefromwbmp($srcfile);
-		break;
-	default:
-		echo '对不起，裁剪图片类型不支持请选择其他类型图片！';
-		exit();
+switch ($srcinfo[2]) {
+    case 1:
+        $imgfrom = imagecreatefromgif($srcfile);
+        break;
+    case 2:
+        $imgfrom = imagecreatefromjpeg($srcfile);
+        break;
+    case 3:
+        $imgfrom = imagecreatefrompng($srcfile);
+        break;
+    case 6:
+        $imgfrom = imagecreatefromwbmp($srcfile);
+        break;
+    default:
+        echo '对不起，裁剪图片类型不支持请选择其他类型图片！';
+        exit();
 }
 
 
@@ -72,30 +70,28 @@ switch($srcinfo[2])
 
 
 //生成图像文件
-switch($srcinfo[2])
-{
-	case 1:
-		imagegif($newimg, $srcfile);
-		break;
-	case 2:
-		imagejpeg($newimg, $srcfile, 100);
-		break;
-	case 3:
-		imagepng($newimg, $srcfile);
-		break;
-	case 6:
-		imagewbmp($newimg, $srcfile);
-		break;
-	default:
-		echo '对不起，裁剪图片类型不支持请选择其他类型图片！';
-		exit();
+switch ($srcinfo[2]) {
+    case 1:
+        imagegif($newimg, $srcfile);
+        break;
+    case 2:
+        imagejpeg($newimg, $srcfile, 100);
+        break;
+    case 3:
+        imagepng($newimg, $srcfile);
+        break;
+    case 6:
+        imagewbmp($newimg, $srcfile);
+        break;
+    default:
+        echo '对不起，裁剪图片类型不支持请选择其他类型图片！';
+        exit();
 }
 
 
 //水印设置
-if($cfg_markswitch=='Y' and $wm=='true')
-{
-	WaterMark($srcfile, PHPMYWIND_ROOT.'/'.$cfg_markpicurl, $cfg_markwhere, $cfg_marktext, '黑体', $cfg_marksize, $cfg_markcolor, $cfg_marktype);
+if ($cfg_markswitch == 'Y' and $wm == 'true') {
+    WaterMark($srcfile, PHPMYWIND_ROOT . '/' . $cfg_markpicurl, $cfg_markwhere, $cfg_marktext, '黑体', $cfg_marksize, $cfg_markcolor, $cfg_marktype);
 }
 
 
